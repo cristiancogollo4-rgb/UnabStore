9d3d47e9a1509c2630331c796a31faee40d13c6e
Cristian Cogollo <cristiancogollo4@gmail.com>
2025-10-15 11:30:48 -0500

    setuo navegation

--- app/src/main/java/com/cristiancogollo/unabstore/MainActivity.kt
+++ app/src/main/java/com/cristiancogollo/unabstore/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.cristiancogollo.unabstore.ui.theme.UnabStoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +21,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            UnabStoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+            val startDestination = "login"
+
+            NavHost(
+                navController = navController,
+                startDestination = startDestination,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                composable(route = "login") {
+                    LoginScreen()
+                }
+                composable(route = "register") {
+                    RegisterScreen()
+                }
+                composable(route = "home") {
+                    HomeScreen()
                 }
             }
         }
     }
 
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        UnabStoreTheme {
-            Greeting("Android")
-        }
-    }
+}
